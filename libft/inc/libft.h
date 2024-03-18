a87a640d563bbbbfc6d2679437249b367b262b89
faboussa <boussard.fanny@gmail.com>
2024-03-18 15:42:30 +0100

    change hasmap struct

--- libft/inc/libft.h
+++ libft/inc/libft.h
@@ -39,8 +39,8 @@ typedef struct s_node
 
 typedef struct s_hashmap_content
 {
-	char					*target; //clef
 	void					*content; // valeur
+	char					*target; //clef
 }	t_hashmap_content;
 
 typedef t_node**	t_hashmap;
