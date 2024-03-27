fa360cd4f438dfafc3259b9d3787929f098c99d4
faboussa <boussard.fanny@gmail.com>
2024-03-27 18:23:38 +0100

    init exit

---
+++ srcs/builtins/exit.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faboussa <faboussa@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 12:49:34 by faboussa          #+#    #+#             */
+/*   Updated: 2024/03/14 12:49:34 by faboussa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+#include "minishell.h"
+#include "utils.h"
+#include "parser.h"
+#include "signals.h"
+# include <readline/history.h>
+
+//The exit utility shall cause the shell to exit from its current
+//execution environment with the exit status specified by the
+//unsigned decimal integer n.  If the current execution environment
+//        is a subshell environment, the shell shall exit from the subshell
+//        environment with the specified exit status and continue in the
+//environment from which that subshell environment was invoked;
+//otherwise, the shell utility shall terminate with the specified
+//exit status. If n is specified, but its value is not between 0
+//and 255 inclusively, the exit status is undefined.
+//
+//A trap on EXIT shall be executed before the shell terminates,
+//except when the exit utility is invoked in that trap itself, in
+//        which case the shell shall exit immediately.
+
+void ft_exit(t_minishell *minishell)
+{
+    if (ft_strcmp(minishell->user_input, )
+}
