da92feb298758723fa81900f4eb2cc7ab55cd524
Boussard Fanny <boussard.fanny@gmail.com>
2024-04-07 22:51:53 +0200

    fixing mixed quotes and without quotes.

--- srcs/lexer/split_string.c
+++ srcs/lexer/split_string.c
@@ -103,7 +103,7 @@ static void deal_with_quotations_marks(char *joined_cmd)
 	}
 }
 
-char **split_with_quotes(char *string)
+char **split_with_quotes(t_minishell *minishell)
 {
 	char *begin;
 	char *joined_string;
@@ -111,7 +111,9 @@ char **split_with_quotes(char *string)
 	size_t i;
 	size_t j;
 	char **temp;
+	char *string;
 
+	string = minishell->user_input;
 	i = 0;
 	while (string[i] && string[i] != '\'' && string[i] != '\"')
 		i++;
@@ -124,9 +126,12 @@ char **split_with_quotes(char *string)
 	}
 
 	i = 0;
-	while (string[i] && string[i] != ' ')
+	while (string[i] == ' ')
 		i++;
-	i++;
+	string = ft_substr(string, i, ft_strlen(string) - i);
+	if (string == NULL)
+		return (NULL);
+
 	begin = malloc(sizeof(char) * i);
 	if (!begin)
 		return (NULL);
@@ -138,43 +143,53 @@ char **split_with_quotes(char *string)
 	if (!joined_string)
 		return (NULL);
 	deal_with_quotations_marks(joined_string);
-	split = ft_split(joined_string, '\"');
+	split = ft_split(string,  '\'');
 	free(joined_string);
 	if (!split)
 		return (NULL);
 	i = 0;
+
+	//split sur espaces
 	while (split[i])
 	{
-			if (ft_strchr(split[i], '\'') == 0 && ft_strchr(split[i], '\"') == 0 && !ft_strnstr_and_check(string, split[i], ft_strlen(string)))
+		if (ft_strchr(split[i], '\'') == 0 && ft_strchr(split[i], '\"') == 0 && !ft_strnstr_and_check(string, split[i], ft_strlen(string)))
+		{
+			char **temp = ft_split(split[i], ' '); // Split sur les espaces
+			if (!temp)
+				return NULL;
+			int k = 0;
+			int temp_size = 0;
+			while (temp[k])
 			{
-					temp = ft_split(split[i], ' '); // Split sur les espaces
-					if (!temp)
-						return NULL;
-					int k = 0;
-					while (temp[k])
-					{
-						split = ft_realloc(split, sizeof(char *) * (i + 2)); // Augmenter la taille du tableau
-						if (!split)
-						{
-							// Gestion de l'échec de la réallocation mémoire
-							perror("Erreur lors de la réallocation de mémoire");
-							exit(EXIT_FAILURE);
-						}
-						split[i++] = temp[k++]; // Ajouter le nouveau token au tableau split
-					}
-					free(temp);
+				temp_size++;
+				k++;
 			}
-			i++;
+			split = realloc(split, (i + temp_size + 1) * sizeof(char *)); // Augmenter la taille du tableau
+			if (!split)
+			{
+				// Gestion de l'échec de la réallocation mémoire
+				perror("Erreur lors de la réallocation de mémoire");
+				return (NULL);
+			}
+			k = 0;
+			while (temp[k])
+			{
+				split[i++] = temp[k++];
+			}
+			free(temp);
 		}
+		i++;
+	}
+	split[i] = NULL;
 	return (split);
 }
 
-	char **split_with_quotes_management(char *string)
+	char **split_with_quotes_management(t_minishell *minishell)
 	{
 		size_t i;
 		char **split;
 
-		split = split_with_quotes(string);
+		split = split_with_quotes(minishell);
 		if (split == NULL)
 			return (NULL);
 		return (split);
