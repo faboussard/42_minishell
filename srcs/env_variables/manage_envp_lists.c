c725b7bcc6b1ed019f71b2064e99ba9e54b938e2
faboussa <boussard.fanny@gmail.com>
2024-04-27 18:29:39 +0200

    unset en cours

--- srcs/env_variables/manage_envp_lists.c
+++ srcs/env_variables/manage_envp_lists.c
@@ -94,26 +94,26 @@ char	*get_env_var_value(char **env, char *var)
 	free_safely_str(tmp);
 	return (NULL);
 }
-
-bool	remove_env_var(t_minishell *minishell, int idx)
-{
-	int	i;
-	int	count;
-
-	if (idx > env_var_count(data->env))
-		return (false);
-	free_safely_str(minishell->env[idx]);
-	i = idx;
-	count = idx;
-	while (minishell->env[i + 1])
-	{
-		minishell->env[i] = ft_strdup(minishell->env[i + 1]);
-		free_safely_str(data->env[i + 1]);
-		count++;
-		i++;
-	}
-	minishell->env = realloc_env_vars(data, count);
-	if (!minishell->env)
-		return (false);
-	return (true);
-}
+//
+//bool	remove_env_var(t_minishell *minishell, int idx)
+//{
+//	int	i;
+//	int	count;
+//
+//	if (idx > env_var_count(data->env))
+//		return (false);
+//	free_safely_str(minishell->env[idx]);
+//	i = idx;
+//	count = idx;
+//	while (minishell->env[i + 1])
+//	{
+//		minishell->env[i] = ft_strdup(minishell->env[i + 1]);
+//		free_safely_str(data->env[i + 1]);
+//		count++;
+//		i++;
+//	}
+//	minishell->env = realloc_env_vars(data, count);
+//	if (!minishell->env)
+//		return (false);
+//	return (true);
+//}
