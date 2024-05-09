50e880641080a19284b2b87f889809529ad8e06a
melodie <melodie.bernard123@yahoo.fr>
2024-05-09 18:31:34 +0200

    fake env no longer returns /home/faboussa instead of the real path the user is in. Thanks to getcwd.

--- srcs/env_variables/fake_env.c
+++ srcs/env_variables/fake_env.c
@@ -14,15 +14,22 @@
 #include "utils.h"
 #include "exec.h"
 
+
 void create_3_env_variables(t_minishell *m)
 {
+	char PWD[PATH_MAX];
 	if (add_new_envp(&m->list_envp,"_=", "/usr/bin/env"))
 		exit_msg_minishell(m, "Malloc failed at create_3_env_variables", -1);
 	m->total_size_envp += ft_strlen("_=") + ft_strlen("/usr/bin/env");
 	if (add_new_envp(&m->list_envp, "SHLVL=", "1"))
 		exit_msg_minishell(m, "Malloc failed at create_3_env_variables", -1);
 	m->total_size_envp += ft_strlen("SHLVL") + ft_strlen("1");
-	if (add_new_envp(&m->list_envp, "PWD=", "/home/faboussa"))
+	if (getcwd(PWD, sizeof(PWD)) == NULL)
+	{
+		perror("minishell: getcwd");
+		ft_strlcpy(PWD, "/home/goinfre/faboussa", 22);
+	}	
+	if (add_new_envp(&m->list_envp, "PWD=", PWD))
 		exit_msg_minishell(m, "Malloc failed at create_3_env_variables", -1);
-	m->total_size_envp += ft_strlen("PWD=") + ft_strlen("/home/faboussa");
+	m->total_size_envp += ft_strlen("PWD=") + ft_strlen(PWD);
 }
