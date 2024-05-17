df6221a5ad2d386ae74b40ea156d0f4c55285f1c
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-05-17 10:59:49 +0200

    seems to work, erasing comments

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -172,7 +172,6 @@ static void	exec_one_cmd(t_minishell *m, t_process_list *pl)
 		m->status = WEXITSTATUS(m->status);
 		close_fds(pl->fd_in, pl->fd_out);
 	}
-	//manage_interrupted_signal(m);
 }
 
 void	execute_cmds(t_minishell *m, size_t nb_cmds)
