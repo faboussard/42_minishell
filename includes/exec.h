baf3bb8e6797c88702eaf51d3c6e6d964ab19027
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-05-21 16:59:53 +0200

    exec better with exit_is_a_directory

--- includes/exec.h
+++ includes/exec.h
@@ -53,6 +53,7 @@ int		handle_in_out(t_minishell *m, t_process_list *pl, int *fd_in);
 void	print_name(t_minishell *m, char *name);
 void	print_name_and_msg(t_minishell *m, char *name, char *msg);
 void	exit_command_not_found(t_minishell *m, char *name, t_process_list *pl);
+void	exit_is_a_directory(t_minishell *m, char *name, t_process_list *pl);
 void	exit_msg_minishell(t_minishell *m, char *msg, int error_code);
 void	print_name_and_exit_perror(t_minishell *m, char *name, int error_code);
 void	print_name_and_exit_msg(t_minishell *m, char *name, char *msg, int err);
--- srcs/exec/errors_exit.c
+++ srcs/exec/errors_exit.c
@@ -26,11 +26,26 @@ void	exit_command_not_found(t_minishell *m, char *name, t_process_list *pl)
 	free_minishell(m);
 	exit(127);
 }
-/*
- * Le dernier pipe doit renvoyer l erreur, le numero ne doit pas capter la premiere
- *
- * */
 
+void	exit_is_a_directory(t_minishell *m, char *name, t_process_list *pl)
+{
+	char *msg;
+	char	*tmp;
+
+	tmp = ft_strjoin("minishell: ", name);
+	if (!tmp)
+		malloc_error_with_exit(m);
+	msg = ft_strjoin(tmp, ": Is a directory");
+	free_safely_str(&tmp);
+	if (!msg)
+		malloc_error_with_exit(m);
+	//	free_safely_str(&name);
+	ft_putendl_fd(msg, 2);
+	free_safely_str(&msg);
+	ft_free_pl_paths(m, pl);
+	free_minishell(m);
+	exit(126);
+}
 void	exit_msg_minishell(t_minishell *m, char *msg, int error_code)
 {
 	ft_putendl_fd(msg, 2);
