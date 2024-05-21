f7e5cb7df8cc268ee9024ebd9037ce068809eb65
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-05-21 16:00:50 +0200

    Behold ! The perfect EXIT has arrived ! And it's normed ! Please do NOT touch it ever again.

--- srcs/builtins/exit_builtin.c
+++ srcs/builtins/exit_builtin.c
@@ -1,62 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exit.c                                               :+:      :+:    :+:   */
+/*   exit_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faboussa <faboussa@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 12:49:34 by faboussa          #+#    #+#             */
-/*   Updated: 2024/03/14 12:49:34 by faboussa         ###   ########.fr       */
+/*   Updated: 2024/05/21 15:50:35 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "utils.h"
 
-//bool str_is_num(char *str)
-//{
-//	int i;
-//
-//	i = 0;
-//	while (str[i])
-//	{
-//		if (ft_isalpha(str[i]))
-//			return (1);
-//		i++;
-//	}
-//	return (0);
-//}
-
-bool str_is_not_num(char *str)
+bool	str_is_not_num(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	if (!str)
+	if (!str || !str[0])
 		return (1);
+	if (str[0] && !(str[1]))
+		return (!ft_isdigit(str[0]));
+	while (str[i] && ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
 	while (str[i])
 	{
-		if (!ft_isdigit(str[i])
-			|| (!ft_isdigit(str[i]) && i == 0 && str[i] != '-' && str[i] != '+')
-			|| ((str[0] == '-' || str[0] == '+') && !str[1]))
+		if (!ft_isdigit(str[i]) || (!ft_isdigit(str[i]) && i == 0
+				&& str[0] != '-' && str[0] != '+'))
 			return (1);
 		i++;
+		while (str[i] && ft_isspace(str[i]))
+			i++;
 	}
 	return (0);
 }
 
-
-void exit_numeric_arg(t_minishell *minishell, char *const *cmd_table, bool is_alpha)
+void	exit_numeric_arg(t_minishell *minishell, char *const *cmd_table,
+		bool is_alpha)
 {
 	if (is_alpha)
 	{
-		print_cmd_perror_no_strerror(cmd_table[1], "exit: numeric argument required\n");
+		print_cmd_perror_no_strerror(cmd_table[1],
+			"exit: numeric argument required\n");
 		free_minishell(minishell);
 		exit(2);
 	}
 }
 
-void exit_no_arg(t_minishell *minishell, char *const *cmd_table)
+void	exit_no_arg(t_minishell *minishell, char *const *cmd_table)
 {
 	if (!cmd_table[1])
 	{
@@ -67,13 +61,15 @@ void exit_no_arg(t_minishell *minishell, char *const *cmd_table)
 
 int	ft_exit(t_minishell *minishell, char **cmd_table)
 {
-	int	exit_code;
+	int		exit_code;
 	bool	is_alpha;
 
 	is_alpha = false;
 	if (minishell->interactive)
 		ft_putendl_fd("exit", 2);
 	exit_no_arg(minishell, cmd_table);
+	if (!cmd_table[1])
+		exit_numeric_arg(minishell, cmd_table, is_alpha);
 	exit_code = ft_atoi_long(cmd_table[1], &is_alpha);
 	if (str_is_not_num(cmd_table[1]))
 		is_alpha = true;
