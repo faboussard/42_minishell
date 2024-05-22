7e45c03b82f485f39d1720acb8212bb847ad7490
faboussa <boussard.fanny@gmail.com>
2024-05-22 15:20:02 +0200

    iterate and remove node corrected ( stncmp wa not going until the ned char)

--- libft/src/ft_str/ft_strncmp.c
+++ libft/src/ft_str/ft_strncmp.c
@@ -12,9 +12,9 @@
 
 #include "../../inc/libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n-- > 0 && *(unsigned char *)s1 + *(unsigned char *)s2 != 0)
+	while (n-- > 0 && (*(unsigned char *)s1 || *(unsigned char *)s2))
 	{
 		if (*(unsigned char *)s1 != *(unsigned char *)s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
--- srcs/env_variables/manage_envp_lists.c
+++ srcs/env_variables/manage_envp_lists.c
@@ -14,9 +14,9 @@
 #include "utils.h"
 #include <string.h>
 
-t_envp_list	*allocate_envp_node(void)
+t_envp_list *allocate_envp_node(void)
 {
-	t_envp_list	*new_envp;
+	t_envp_list *new_envp;
 
 	new_envp = ft_calloc(1, sizeof(t_envp_list));
 	if (new_envp == NULL)
@@ -28,7 +28,7 @@ t_envp_list	*allocate_envp_node(void)
 	return (new_envp);
 }
 
-bool	init_envp_node(t_envp_list *new_envp, char *target, char *content)
+bool init_envp_node(t_envp_list *new_envp, char *target, char *content)
 {
 	new_envp->target = ft_strdup(target);
 	if (new_envp->target == NULL)
@@ -51,9 +51,9 @@ bool	init_envp_node(t_envp_list *new_envp, char *target, char *content)
 	return (true);
 }
 
-t_envp_list	*create_new_envp(char *target, char *content)
+t_envp_list *create_new_envp(char *target, char *content)
 {
-	t_envp_list	*new_envp;
+	t_envp_list *new_envp;
 
 	new_envp = allocate_envp_node();
 	if (new_envp == NULL)
@@ -70,17 +70,44 @@ char *trim_equal_sign(char *key)
 {
 	char *trimmed_key;
 
-	trimmed_key= ft_strtrim(key, "=");
+	trimmed_key = ft_strtrim(key, "=");
 	if (trimmed_key == NULL)
 		return (NULL);
 	return (trimmed_key);
 }
 
-int	remove_env_var(t_envp_list **env, char *key)
+int iterate_and_remove_node(t_envp_list **env, char *key)
 {
-	char 		*key_without_equal_sign;
-	char		*target_without_equal_sign;
-	t_envp_list	*cpy;
+	char *target_without_equal_sign;
+	t_envp_list *cpy;
+	size_t len;
+
+	len = ft_strlen(key) + 1;
+	cpy = *env;
+	while (cpy)
+	{
+		target_without_equal_sign = trim_equal_sign(cpy->target);
+		if (target_without_equal_sign == NULL)
+		{
+			free_safely_str(&key);
+			return (MALLOC_FAILED);
+		}
+		if (ft_strncmp(key, target_without_equal_sign, len) == 0)
+		{
+			remove_node_envp(env, cpy);
+			free_safely_str(&target_without_equal_sign);
+			break;
+		}
+		else
+			cpy = cpy->next;
+		free_safely_str(&target_without_equal_sign);
+	}
+	return (0);
+}
+
+int remove_env_var(t_envp_list **env, char *key)
+{
+	char *key_without_equal_sign;
 
 	key_without_equal_sign = NULL;
 	if (ft_strchr(key, '='))
@@ -89,34 +116,27 @@ int	remove_env_var(t_envp_list **env, char *key)
 		if (key_without_equal_sign == NULL)
 			return (MALLOC_FAILED);
 	}
-	cpy = *env;
-	while (cpy)
+	if (key_without_equal_sign)
 	{
-		target_without_equal_sign = trim_equal_sign(cpy->target);
-		if (target_without_equal_sign == NULL)
+		if (iterate_and_remove_node(env, key_without_equal_sign) == MALLOC_FAILED)
 		{
 			free_safely_str(&key_without_equal_sign);
 			return (MALLOC_FAILED);
 		}
-		if (ft_strncmp(key, target_without_equal_sign, ft_strlen(key)) == 0
-			|| (key_without_equal_sign && ft_strncmp(key_without_equal_sign, target_without_equal_sign, ft_strlen(key_without_equal_sign)) == 0))
-		{
-			remove_node_envp(env, cpy);
-			free_safely_str(&target_without_equal_sign);
-			break ;
-		}
-		else
-			cpy = cpy->next;
-		free_safely_str(&target_without_equal_sign);
+	}
+	else
+	{
+		if (iterate_and_remove_node(env, key) == MALLOC_FAILED)
+		return (MALLOC_FAILED);
 	}
 	free_safely_str(&key_without_equal_sign);
 	return (0);
 }
 
-void	remove_node_envp(t_envp_list **begin_list, t_envp_list *node_to_remove)
+void remove_node_envp(t_envp_list **begin_list, t_envp_list *node_to_remove)
 {
-	t_envp_list	*current;
-	t_envp_list	*previous_node;
+	t_envp_list *current;
+	t_envp_list *previous_node;
 
 	current = *begin_list;
 	previous_node = NULL;
@@ -135,7 +155,7 @@ void	remove_node_envp(t_envp_list **begin_list, t_envp_list *node_to_remove)
 	}
 }
 
-void	free_envp(t_envp_list *envp)
+void free_envp(t_envp_list *envp)
 {
 	if (envp != NULL)
 	{
