0d088a5524db80caa8c890363460871bc03df854
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-05-23 11:12:50 +0200

    double dirs

---
+++ srcs/expansio_parsing/expansion_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expansion.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 12:49:34 by faboussa          #+#    #+#             */
+/*   Updated: 2024/04/11 17:01:49 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+#include "utils.h"
+#include "parser.h"
+
+// HELLO
+char *expand_sign(char *string, char *temp)
+{
+	while (*string && *string != '=')
+		string++;
+	string = ft_strjoin(temp, string);
+	if (string == NULL)
+		exit_msg(NULL, "Malloc failed at expand_sign", ENOMEM);
+	return (string);
+}
+
+void treat_in_delimitor(t_token_list **iterator)
+{
+	if ((*iterator)->e_operator == DOLLAR)
+		(*iterator)->e_operator = 0;
+	(*iterator)->is_quoted_delimiter = true;
+	(*iterator) = (*iterator)->next;
+}
+
+void handle_delimitor(t_token_list **iterator)
+{
+	*iterator = (*iterator)->next;
+	while ((*iterator) && (*iterator)->e_operator == IS_SPACE)
+		(*iterator) = (*iterator)->next;
+	while ((*iterator) && (*iterator)->next && (*iterator)->e_operator != IS_SPACE)
+	{
+		if ((*iterator)->e_operator == DOLLAR)
+			(*iterator)->e_operator = 0;
+		while ((*iterator) && ((*iterator)->e_operator == DOUBLE_QUOTE || (*iterator)->e_operator == SINGLE_QUOTE))
+		{
+			(*iterator)->is_quoted_delimiter = true;
+			(*iterator) = (*iterator)->next;
+			while ((*iterator) && (*iterator)->e_operator != DOUBLE_QUOTE &&
+					(*iterator)->e_operator != SINGLE_QUOTE)
+			{
+				(*iterator)->is_quoted_delimiter = true;
+				(*iterator) = (*iterator)->next;
+			}
+		}
+		if ((*iterator))
+			treat_in_delimitor(iterator);
+	}
+}
+
+void change_name_to_status(t_minishell *minishell, t_token_list *iterator)
+{
+	join_tokens(minishell, &iterator);
+	free_safely_str(&(iterator->name));
+	iterator->name = ft_itoa(minishell->status);
+	if (iterator->name == NULL)
+		exit_msg(minishell, "Malloc failed at expander", ENOMEM);
+}
+
+char *expand_sigil(char *string, t_minishell *minishell)
+{
+	char *final_string;
+
+	if (ft_isdigit(*string))
+		final_string = ft_strdup(string + 1);
+	else
+		final_string = identify_envp_string(string, minishell);
+	return (final_string);
+}
---
+++ srcs/joiner_parsing/join_tokens.c
@@ -0,0 +1,163 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenization.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faboussa <faboussa@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 12:49:34 by faboussa          #+#    #+#             */
+/*   Updated: 2024/05/02 09:50:06 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "lexer.h"
+#include "utils.h"
+#include "parser.h"
+
+// KOEJN
+
+void join_tokens(t_minishell *minishell, t_token_list **list)
+{
+	char *joined_name;
+	t_token_list *t2;
+	t_token_list *t1;
+
+	t1 = (*list);
+	t2 = (*list)->next;
+	joined_name = ft_strjoin(t1->name, t2->name);
+	if (joined_name == NULL)
+		exit_msg(minishell, "Malloc failed at tokenization", ENOMEM);
+	free_safely_str(&(t1->name));
+	t1->name = ft_strdup(joined_name);
+	if (t1->name == NULL)
+		exit_msg(minishell, "Malloc failed at tokenization", ENOMEM);
+	free_safely_str(&joined_name);
+	if (t1->name == NULL)
+		exit_msg(minishell, "Malloc failed at tokenization", ENOMEM);
+	del_next_token(&t1);
+	define_token_types(COMMAND, NO_BUILTIN, NO_OPERATOR, t1);
+}
+
+void join_between_quotes_handler(t_minishell *minishell, t_token_list **list, enum e_token_operators op)
+{
+	int count;
+
+	count = 1;
+	if (*list == NULL || (*list)->next == NULL || (*list)->next->next == NULL)
+		return;
+	(*list) = (*list)->next;
+	while ((*list) && (*list)->next)
+	{
+		if ((*list)->next->e_operator == op)
+		{
+			define_token_types(COMMAND, NO_BUILTIN, NO_OPERATOR, (*list));
+			(*list) = (*list)->next;
+			(*list) = (*list)->next;
+			count++;
+		}
+		if (count == 2)
+			return;
+		else if (count == 1)
+			join_tokens(minishell, list);
+	}
+}
+
+void join_between_quotes(t_minishell *minishell, t_token_list **list)
+{
+	t_token_list *cpy;
+	int quote_type;
+
+	cpy = *list;
+	while (*list != NULL && (*list)->next != NULL)
+	{
+		quote_type = (*list)->e_operator;
+		if ((quote_type == DOUBLE_QUOTE || quote_type == SINGLE_QUOTE) && quote_type == (int)(*list)->next->e_operator)
+		{
+			join_tokens(minishell, list);
+			change_token_name(minishell, list, "\0");
+			continue ;
+		}
+		else if ((quote_type == DOUBLE_QUOTE || quote_type == SINGLE_QUOTE) && check_if_more_tokens(list, quote_type))
+		{
+			join_between_quotes_handler(minishell, list, quote_type);
+			if ((*list) == NULL)
+				break;
+		}
+		else
+			(*list) = (*list)->next;
+	}
+	*list = cpy;
+}
+
+/// OHEEEE
+
+
+
+// change ca
+
+// oheheheee
+int no_space_in_list(t_token_list *list)
+{
+	while (list != NULL)
+	{
+		if (list->e_operator == IS_SPACE)
+			return (0);
+		list = list->next;
+	}
+	return (1);
+}
+
+void join_between_spaces(t_minishell *minishell, t_token_list **list)
+{
+	t_token_list *cpy;
+
+	if (list == NULL || *list == NULL || no_space_in_list(*list))
+		return ;
+	cpy = *list;
+	while (*list != NULL && (*list)->next != NULL)
+	{
+		while ((*list) && (*list)->e_operator == IS_SPACE)
+			*list = (*list)->next;
+		if ((*list) != NULL && (*list)->next != NULL && (*list)->next->e_operator != IS_SPACE)
+		{
+			if (is_redirect_token_or_pipe((*list)->next) || is_redirect_token_or_pipe(*list))
+				*list = (*list)->next;
+			else if ((*list)->e_operator != IS_SPACE)
+			{
+				join_tokens(minishell, list);
+				continue ;
+			}
+		}
+		if ((*list) == NULL)
+			break ;
+		*list = (*list)->next;
+	}
+	*list = cpy;
+}
+
+char *join_all(t_minishell *minishell, t_token_list **list)
+{
+	t_token_list *iterator;
+	char *new_table;
+	char *temp;
+	size_t total_length;
+
+	temp = NULL;
+	new_table = NULL;
+	total_length = 0;
+	iterator = *list;
+	while (iterator)
+	{
+		total_length += ft_strlen(iterator->name);
+		iterator = iterator->next;
+	}
+	new_table = ft_calloc(total_length + 1, sizeof(char));
+	if (new_table == NULL)
+		exit_msg(minishell, "Memory allocation failed for command table array", ENOMEM);
+	iterator = *list;
+	while (iterator)
+		join_token_name(minishell, temp, &iterator, &new_table);
+	return (new_table);
+}
+
